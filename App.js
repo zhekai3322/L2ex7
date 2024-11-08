c11627711b36e5db414438f9affcdc1579e4906b
zhekai3322 <23024477@myrp.edu.sg>
2024-11-08 11:12:13 +0800

    L04 Ex 5

--- App.js
+++ App.js
@@ -1,33 +1,97 @@
-import React from 'react';
-import {View, ScrollView} from 'react-native';
-import Boats from './components/Boats';
+// import React from 'react';
+// import {View, ScrollView} from 'react-native';
+// import Boats from './components/Boats';
+//
+//
+// const AllBoats = () => {
+//     return (
+//         <ScrollView>
+//             <Boats
+//                 name= 'Princess V48'
+//                 description= 'There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior.'
+//                 picture= {require('./img/princess.jpg')}
+//             />
+//             <Boats
+//                 name= 'Bayliner 175 Bowrider'
+//                 description= 'Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance.'
+//                 picture= {require('./img/bayliner.jpg')}
+//             />
+//         </ScrollView>
+//     );
+// };
+//
+//
+//
+// const App = () => {
+//     return (
+//         <View style={{ flex: 1, padding: 20 }}>
+//             <AllBoats />
+//         </View>
+//     );
+// };
+//
+// export default App;
 
+//L04 Ex5
+import React from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import Boats from './components/Boats';
 
 const AllBoats = () => {
     return (
-        <ScrollView>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
             <Boats
-                name= 'Princess V48'
-                description= 'There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior.'
-                picture= {require('./img/princess.jpg')}
+                name='Sea Ray 500 Sundancer'
+                description='Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away.'
+                picture={require('./img/sea_ray.jpg')}
             />
             <Boats
-                name= 'Bayliner 175 Bowrider'
-                description= 'Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance.'
-                picture= {require('./img/bayliner.jpg')}
+                name='Four Winns Horizon 180'
+                description='A sporty look and refined details truly set the Horizon 180 above all others.'
+                picture={require('./img/four_winns.jpg')}
+            />
+            <Boats
+                name='Flipper 640 ST'
+                description='A modern take on the classic, traditional hardtop and perfect for a family picnic.'
+                picture={require('./img/flipper.jpg')}
+            />
+            <Boats
+                name='Princess V48'
+                description='There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior.'
+                picture={require('./img/princess.jpg')}
+            />
+            <Boats
+                name='Bayliner 175 Bowrider'
+                description='Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance.'
+                picture={require('./img/bayliner.jpg')}
+            />
+            <Boats
+                name='Fairline Targa 47'
+                description='Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit.'
+                picture={require('./img/fairline.jpg')}
             />
         </ScrollView>
     );
 };
 
-
-
 const App = () => {
     return (
-        <View style={{ flex: 1, padding: 20 }}>
+        <View style={styles.container}>
             <AllBoats />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 20,
+        paddingTop: 40,
+        backgroundColor: '#e6c066',
+    },
+    scrollContainer: {
+        paddingBottom: 20,
+    },
+});
 
 export default App;
--- components/Boats.js
+++ components/Boats.js
@@ -1,21 +1,69 @@
-import { View, Text, Image } from "react-native";
+// import { View, Text, Image } from "react-native";
+// import React from "react";
+//
+// const Boats = ({ name, description, picture }) => {
+//     return (
+//         <View>
+//             <Image source={picture} style={{width:400, height:500}} />
+//             <View>
+//                 <Text>
+//                     {name}
+//                 </Text>
+//                 <Text>
+//                     {description}
+//                 </Text>
+//             </View>
+//         </View>
+//     );
+// };
+//
+//
+// export default Boats;
+
+//L04 Ex5
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 
 const Boats = ({ name, description, picture }) => {
     return (
-        <View>
-            <Image source={picture} style={{width:400, height:500}} />
-            <View>
-                <Text>
-                    {name}
-                </Text>
-                <Text>
-                    {description}
-                </Text>
+        <View style={styles.container}>
+            <Image source={picture} style={styles.image} />
+            <View style={styles.textContainer}>
+                <Text style={styles.boatName}>{name}</Text>
+                <Text style={styles.boatDescription}>{description}</Text>
             </View>
         </View>
     );
 };
 
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 20,
+        borderRadius: 10,
+        overflow: 'hidden',
+        backgroundColor: '#fff',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    image: {
+        width: '100%',
+        height: 300,
+    },
+    textContainer: {
+        padding: 15,
+    },
+    boatName: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    boatDescription: {
+        fontSize: 16,
+        color: '#555',
+    },
+});
 
 export default Boats;
